722f616aa5d257605d5b916a86a22b8542c8a44c
Raphael Barth <raphael.t.barth+github@gmail.com>
2022-12-19 17:10:25 +0100

    tap pane anfügen und menü auslagern

--- jfxNotenplaner/src/main/java/de/pbma/java/MainGUIController.java
+++ jfxNotenplaner/src/main/java/de/pbma/java/MainGUIController.java
@@ -1,38 +1,8 @@
 package de.pbma.java;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.MenuBar;
+
 
 public class MainGUIController {
-	private MenuBarLogic menuBarLogic;
-
-	public MainGUIController() {
-		this.menuBarLogic = new MenuBarLogic();
-	}
-
-	@FXML
-	private void handleLoadAction(ActionEvent ae) {
-		menuBarLogic.fileLoad();
-	}
-
-	@FXML
-	private void handleSaveAction(ActionEvent ae) {
-		menuBarLogic.fileSave();
-	}
-
-	@FXML
-	private void handleNewAction(ActionEvent ae) {
-		menuBarLogic.fileNew();
-	}
-
-	@FXML
-	private void handleLightAction(ActionEvent ae) {
-		menuBarLogic.settingsMode(true);
-	}
-
-	@FXML
-	private void handleDarkAction(ActionEvent ae) {
-		menuBarLogic.settingsMode(false);
-	}
+	
+	
 }

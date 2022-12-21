dbbf8b4d53d32c8cba58d6a70f07b8a9cc339734
n2160482 <2160482@stud.hs-mannheim.de>
2022-12-21 11:48:26 +0100

    parser fertig stellen

---
+++ jfxNotenplaner/src/main/java/de/pbma/java/ParserException.java
@@ -0,0 +1,10 @@
+package de.pbma.java;
+
+public class ParserException extends Exception {
+
+	private static final long serialVersionUID = 7910297747554122144L;
+	public ParserException(String msg) {
+		super(msg);
+	}
+
+}

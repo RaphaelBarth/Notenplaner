56ef75b51a3cc5b47d545f64366922325c65ecc8
Student 2160482 <n2160482@x2go>
2023-01-02 12:03:26 +0100

    kleine verbesserungen

--- jfxNotenplaner/src/main/java/de/pbma/java/ModuleEntry.java
+++ jfxNotenplaner/src/main/java/de/pbma/java/ModuleEntry.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
@@ -20,18 +22,18 @@ public class ModuleEntry implements Serializable {
 	public static final String CPS = "cps";
 	public static final String NOTE = "grade";
 
-	private final static List<String> normalEvaluation = FXCollections
-			.observableArrayList(Arrays.asList(Grades.NOTPASSED.toString(), Grades.PASSED.toString()));
-	private final static List<String> gradeEvaluation = FXCollections.observableArrayList(Stream.of(Grades.values())
-			.filter(g -> g != Grades.PASSED).map(g -> g.toString()).sorted().collect(Collectors.toList()));
+	private final static List<Grades> normalEvaluation = FXCollections
+			.observableArrayList(Arrays.asList(Grades.NOTPASSED, Grades.PASSED));
+	private final static List<Grades> gradeEvaluation = FXCollections.observableArrayList(Stream.of(Grades.values())
+			.filter(g -> g != Grades.PASSED).sorted().collect(Collectors.toList()));
 
 	private ModuleEntryData entryData;
-	private StringProperty gradeProperty;
+	private ObjectProperty<Grades> gradeProperty;
 	private StringProperty nameProperty;
 
 	public ModuleEntry(ModuleEntryData entryData) {
 		this.entryData = entryData;
-		this.gradeProperty = new SimpleStringProperty();
+		this.gradeProperty = new SimpleObjectProperty<Grades>();
 		this.nameProperty = new SimpleStringProperty();
 		this.nameProperty.setValue(entryData.getSubjectName());
 		this.gradeProperty.setValue(entryData.getGrade());
@@ -67,16 +69,16 @@ public class ModuleEntry implements Serializable {
 		return this.entryData.getCps();
 	}
 
-	public String getGrade() {
+	public Grades getGrade() {
 		return this.gradeProperty.getValue();
 	}
 
-	public void setGrade(String grade) {
+	public void setGrade(Grades grade) {
 		this.gradeProperty.setValue(grade);
 		this.entryData.setGrade(grade);
 	}
 
-	public List<String> getPossibleEvaluations() {
+	public List<Grades> getPossibleEvaluations() {
 		return (this.entryData.hasGradeAsEvaluation()) ? gradeEvaluation : normalEvaluation;
 	}
 
@@ -106,7 +108,7 @@ public class ModuleEntry implements Serializable {
 		return Objects.equals(entryData, other.entryData);
 	}
 
-	public StringProperty getGradeProperty() {
+	public ObjectProperty<Grades> getGradeProperty() {
 		return gradeProperty;
 	}
 
